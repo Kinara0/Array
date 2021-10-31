d96f2c6064db5f463cbad8651133570171779582
Kinara0 <kinarasoni@yahoo.com>
2021-10-30 22:03:40 -0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         // Check whether an array contains a given number or not
-        int arr[] = {2, 4, 6, 3, 6, 5, 66};
+        int arr[] = {12, 4, 6, 3, 6, 5, 66};
         System.out.println(contains(arr, 4));
+        //find the first index of a given element in an array, if not found return -1
+        System.out.println(firstIndexOf(arr, 10));
+        //find the maximum value in an array
+        System.out.println(min(arr));
+
+
     }
 
     public static boolean contains(int[] arr, int value) {
@@ -16,5 +24,26 @@ public class Main {
 
         }
         return false;
+    }
+    public static int firstIndexOf(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==value) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+    public static int min(int[] arr) {
+// find max value from an array
+        int min =arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<min) {
+                min = arr[i];
+
+            }
+
+        }
+        return min;
     }
 }
